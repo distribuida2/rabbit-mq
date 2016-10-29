09244fe5e57ddba5511916e0665c725fa6c6f77d
Juan Lagostena <jlagostena@bitsense.com.ar>
2016-10-29 12:03:45 -0300

    Primeros tres ejemplos de Rabbit + README

---
+++ 01.hello-world/consumer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import pika
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+
+connection = pika.BlockingConnection()
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+
+channel.basic_consume(callback,
+                      queue='hello',
+                      no_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
---
+++ README.md
@@ -0,0 +1,16 @@
+# Rabbit MQ
+
+
+## Artículos interesantes
+
+- [AMQP 0-9-1 explicado](http://www.rabbitmq.com/tutorials/amqp-concepts.html)
+- [RabbitMQ en Mercado Libre](http://blogs.vmware.com/vfabric/2013/01/messaging-architecture-using-rabbitmq-at-the-worlds-8th-largest-retailer.html)
+
+
+## Consola de Rabbit
+
+- Exchanges declarados
+>rabbitmqctl list_exchanges
+
+- Bindings de las listas
+>rabbitmqctl list_bindings
