bde42e6c5b4ad4467a2810de7086918570eeee08
Juan Lagostena <jplagostena@undav.edu.ar>
2022-05-26 15:50:00 -0300

    Pequeñas modificaciones en primer ejemplo + agregado de excepción y catch en el momento de la ejecución

--- 01.hello-world/consumer.py
+++ 01.hello-world/consumer.py
@@ -4,7 +4,7 @@ import pika
 
 
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+    print(" [x] Received %r" % str(body))
 
 
 connection = pika.BlockingConnection()
@@ -12,7 +12,11 @@ channel = connection.channel()
 channel.queue_declare(queue='hello')
 
 channel.basic_consume(on_message_callback=callback,
-                      queue='hello', auto_ack=True)
+                      queue='hello')
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
+try:
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+except KeyboardInterrupt:
+    channel.stop_consuming()
+connection.close()
