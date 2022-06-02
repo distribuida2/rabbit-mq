7ecce5edbadfaacbe521cd03ec3c0ca5e208b249
Juan Lagostena <jplagostena@undav.edu.ar>
2022-06-02 18:22:34 -0300

    Agregado ejemplo 5 del tutorial de Rabbit

---
+++ 05.topics/receive_logs_topic.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import pika
+import sys
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
+
+result = channel.queue_declare(queue='', exclusive=True)
+queue_name = result.method.queue
+
+# update.price.title
+# update.price
+# update.picture.address
+
+binding_keys = sys.argv[1:]
+
+if not binding_keys:
+    sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
+    sys.exit(1)
+
+for binding_key in binding_keys:
+    channel.queue_bind(
+        exchange='topic_logs', queue=queue_name, routing_key=binding_key)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] %r:%r" % (method.routing_key, body))
+
+
+channel.basic_consume(
+    queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+try:
+    print(' [*] Esperando por mensajes. Para salir presione CTRL+C')
+    channel.start_consuming()
+except KeyboardInterrupt:
+    channel.stop_consuming()
+connection.close()
